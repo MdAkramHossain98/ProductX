aa1e7d0c347479ec18db80e460b0dbfda6821474
Akram Hossain Fahim <73392407+AkramHossain97@users.noreply.github.com>
2026-09-14 19:06:49 +1000

    Initial Commit

---
+++ ProductX/ProductXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductXApp.swift
+//  ProductX
+//
+//  Created by Akram on 14/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ProductXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProductXTests/ProductXTests.swift
@@ -0,0 +1,19 @@
+//
+//  ProductXTests.swift
+//  ProductXTests
+//
+//  Created by Akram on 14/9/26.
+//
+
+import Testing
+@testable import ProductX
+
+struct ProductXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
